21af377faa6530da91814734438931f0009fca27
Daniel Yip <daniel.yip1@nhs.net>
2023-12-06 16:08:47 +0000

    Removed useless import

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -3,7 +3,6 @@ See
 https://github.com/NHSDigital/pytest-nhsd-apim/blob/main/tests/test_examples.py
 for more ideas on how to test the authorization of your API.
 """
-import time
 import json
 import os
 import pytest

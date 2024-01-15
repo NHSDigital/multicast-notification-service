3b30f5c4eed11182709e15a2cd5197389729ccfe
jamesthompson26-nhs <james.thompson26@nhs.net>
2024-01-15 10:13:15 +0000

    [EM-714] - Adding MESH Mailbox Custom Attributes

--- proxies/live/apiproxy/resources/jsc/ConnectionMetadata.SetAuthorizationHeader.js
+++ proxies/live/apiproxy/resources/jsc/ConnectionMetadata.SetAuthorizationHeader.js
@@ -1,2 +1,5 @@
 const appPermissions = context.getVariable('app.permissions');
 context.targetRequest.headers['X-MNS-Application-Permissions'] = appPermissions;
+
+const appMeshMailboxes = context.getVariable('app.meshmailboxes');
+context.targetRequest.headers['X-MNS-Application-Mesh-Mailboxes'] = appMeshMailboxes;

8b06cb896cac384f3fccdaf93d99bf5fc5e47f2f
jimasp <464080+jimasp@users.noreply.github.com>
2024-04-08 12:48:24 +0100

    EM-844 remove which check as it could be python which would be .venv path

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -36,14 +36,3 @@ if [ "$RESHIM" -eq 1 ]; then
     asdf reshim
 fi
 
-FIRST_PLUGIN="$(echo "$1" | cut -d' ' -f1)"
-
-if which "$FIRST_PLUGIN" | grep -q "$HOME/.asdf"; then
-    echo "asdf installed tooling successfully"
-    exit 0
-fi
-
-echo "*** ERROR: asdf environment/installation failed! ***"
-echo "'which $FIRST_PLUGIN' does not contain path $HOME/.asdf"
-echo "(check asdf shims come before other package/version managers in your PATH)"
-exit 1

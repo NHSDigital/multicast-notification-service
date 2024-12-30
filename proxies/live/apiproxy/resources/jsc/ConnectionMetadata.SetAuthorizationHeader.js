78c5a29e7325f2f13be5a4672c5705df0a86674c
Jon Marston <john.marston1@nhs.net>
2024-12-30 15:17:26 +0000

    EM-1579: remove references to the mesh-mailbox attribute.

--- proxies/live/apiproxy/resources/jsc/ConnectionMetadata.SetAuthorizationHeader.js
+++ proxies/live/apiproxy/resources/jsc/ConnectionMetadata.SetAuthorizationHeader.js
@@ -8,6 +8,3 @@
 
 const appPermissions = context.getVariable('app.permissions');
 context.targetRequest.headers['X-MNS-Application-Permissions'] = appPermissions;
-
-const appMeshMailboxes = context.getVariable('app.mesh-mailboxes');
-context.targetRequest.headers['X-MNS-Application-Mesh-Mailboxes'] = appMeshMailboxes;
